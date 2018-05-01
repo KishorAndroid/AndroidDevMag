a4073288d31816889d8c5d56db1217d8b64abd13
kishorandroid <kishordahiwadkar@gmail.com>
2018-05-02 01:36:13 +0530

    - try kotlin coroutines with anko

--- app/src/publisher/java/com/kishordahiwadkar/androidevmag/publisher/MainActivity.kt
+++ app/src/publisher/java/com/kishordahiwadkar/androidevmag/publisher/MainActivity.kt
@@ -14,6 +14,9 @@ import com.kishordahiwadkar.androidevmag.R
 import com.kishordahiwadkar.androidevmag.models.NewsItem
 import kotlinx.android.synthetic.publisher.activity_main.*
 import kotlinx.android.synthetic.publisher.content_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
@@ -35,7 +38,12 @@ class MainActivity : AppCompatActivity() {
 
         if (Intent.ACTION_SEND == action && type != null) {
             if ("text/plain" == type) {
-                parseDocument(intent.getStringExtra(Intent.EXTRA_TEXT))
+                //parseDocument(intent.getStringExtra(Intent.EXTRA_TEXT))
+
+                async (UI) {
+                    val result = bg { run(intent.getStringExtra(Intent.EXTRA_TEXT)) }
+                    result.await()
+                }
             }
         }
 
@@ -49,6 +57,56 @@ class MainActivity : AppCompatActivity() {
     private fun parseDocument(stringExtra: String?) {
         val thread = SimpleThread(stringExtra!!)
         thread.start()
+    }
+
+    private fun run(stringExtra: String) {
+        try {
+
+            var title: String?
+            var description = ""
+
+            Log.d(tag, stringExtra)
+
+            val document: Document = Jsoup.connect(stringExtra).get()
+
+            title = document.title()
+
+            val metaTags = document.getElementsByTag("meta")
+
+            for (metaTag: Element in metaTags) {
+                val content = metaTag.attr("content")
+                val name = metaTag.attr("name")
+
+                if (name == "og:description" || name == "description") {
+                    description = content
+                }
+            }
+
+            Log.d(tag, title)
+            Log.d(tag, description)
+
+            val imageTags = document.getElementsByTag("img")
+
+            for (imageTag: Element in imageTags) {
+                val src = imageTag.absUrl("src")
+                if (src.contains(".png", true) ||
+                        src.contains(".jpg", true) ||
+                        src.contains(".jpeg", true)) {
+                    Log.d(tag, src)
+                    imageList.add(src)
+                }
+            }
+
+            this@MainActivity.runOnUiThread({
+                editTitle.setText(title, TextView.BufferType.EDITABLE)
+                editDescription.setText(description, TextView.BufferType.EDITABLE)
+                rvDocumentImages.adapter = ImagesAdapter(imageList, this@MainActivity, imageAdapterClickListener)
+                rvDocumentImages.layoutManager = android.support.v7.widget.StaggeredGridLayoutManager(2, android.support.v7.widget.StaggeredGridLayoutManager.VERTICAL)
+            })
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     inner class SimpleThread(private val stringExtra: String) : Thread() {
